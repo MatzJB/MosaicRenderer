dbc1f17271f373d5b22230b0c1a5faba86960fc0
MatzJB <matz.johansson@gmail.com>
2017-08-28 22:04:59 +0200

    added support to convert a index matrix to a mosaic

---
+++ renderer/mosaicIndexToImage.m
@@ -0,0 +1,52 @@
+%mosaicIndexToImage(modelsDir, frameFormat, indices, offset, palette, gray)
+%
+%  mosaicIndexToImage takes the index matrix of a mosaic along with an
+%  <offset> and a <palette> and returns the mosaic matrix. If <gray> is set to
+%  true, the mosaic will be gray scale.
+%
+%  If frames from a movie is used as mosels and a movie sequence is to be
+%  shown, an optimization is to only collect a subset of mosels but still
+%  step in the frames of the movie at 30FPS. The benefit is that not all
+%  frames are used in the palette which will speed up the render process.
+
+function mosaic = mosaicIndexToImage(moselsDir, frameFormat, indices, offset, palette, gray)
+
+if gray, palette = paletteToGray(palette); end
+
+[ri, ci, ~] = size(indices);
+[rp, cp, ~] = size(palette(1).data);
+rMosaic = ri*rp;
+cMosaic = ci*cp;
+mosaic = zeros(rMosaic, cMosaic, 3);
+mosDim = size(palette(1).data);
+paletteLength = length(palette);
+
+for y = 1:mosDim(1):rMosaic - mosDim(1)
+    for x = 1:mosDim(2):cMosaic - mosDim(2)
+        iIndex = round( y/mosDim(1) );
+        jIndex = round( x/mosDim(2) );
+        ind = indices(iIndex, jIndex);
+        
+        if offset~=1  % read frame using name of sample
+            moselName = palette(ind).name;
+            nameParts = strsplit(moselName, '_');
+            frame = strsplit(nameParts{2}, '.');
+            frameId = offset + str2double(frame{1});
+            frameId = 1 + mod(frameId,paletteLength); %cycling frame
+            str = sprintf(frameFormat, frameId);
+            newMoselName = [moselsDir, nameParts{1}, '_', str, '.jpg'];
+            im = imread(newMoselName);
+            tile = rescaleAndCrop(im, [rp, cp]);
+        else
+            tile = palette(ind).data;
+        end
+        
+        if gray
+            tile = rgb2gray(tile);
+            tile = cat(3, tile, tile, tile);
+        end
+        
+        mosaic(iIndex*mosDim(1)+1:(iIndex+1)*mosDim(1), jIndex*mosDim(2)+1:(jIndex+1)*mosDim(2), :) = tile;
+    end
+end
+
